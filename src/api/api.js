830d727fcf1519447076e6884fa3b710bf6aae1c
KrasnovDaniil1 <kd69931@gmail.com>
2024-11-26 12:47:49 +0200

    add

--- src/api/api.js
+++ src/api/api.js
@@ -6,9 +6,10 @@ const moexBondsUrl = `${moexBaseUrl}/engines/stock/markets/bonds/securities.json
 export const getBonds = async () => {
     const response = await fetch(moexBondsUrl);
     const data = await response.json();
-    const columns = data.securities.columns;
+    const columnsSecurities = data.securities.columns;
+    const dataSecurities = data.securities.data;
     const securities = data.securities.data;
-    return changeBonds(securities, columns);
+    const columnsMarketData = data.marketdata.columns;
+    const dataMarketData = data.marketdata.data;
+    return changeBonds(dataSecurities, columnsSecurities);
 };
-
-

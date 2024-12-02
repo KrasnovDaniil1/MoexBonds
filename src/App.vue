412f963d91520951fb62c870f4b75636a9edbe33
TimurEfimov <tefimov85@gmail.com>
2024-12-02 23:31:20 +0300

    searchHTML

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Header from "./components/Header.vue";
 import Table from "./components/Table.vue";
 import Loader from "./components/Loader.vue";
 import Sort from "./components/Sort.vue";
+import Search from "./components/Search.vue";
 
 import { onMounted, ref, watch } from "vue";
 import { getBonds } from "./api/api";
@@ -16,40 +17,40 @@ const sortKey = ref(sortList.default);
 const sortUpDown = ref(sortByUpDown.default);
 
 function SortBonds() {
-    data.value = SortNum(data.value, sortKey.value, sortUpDown.value);
+  data.value = SortNum(data.value, sortKey.value, sortUpDown.value);
 }
 
 onMounted(async () => {
-    data.value = await getBonds();
-    SortBonds();
-    loading.value = false;
+  data.value = await getBonds();
+  SortBonds();
+  loading.value = false;
 });
 
 function changeSortUpDown(v) {
-    sortUpDown.value = !sortUpDown.value;
-    SortBonds();
+  sortUpDown.value = !sortUpDown.value;
+  SortBonds();
 }
 
 function changeSortKey(v) {
-    sortKey.value = v;
-    SortBonds();
+  sortKey.value = v;
+  SortBonds();
 }
-
 </script>
 
 <template>
-    <Header />
-    <Loader :loading="loading" />
-    <div>
-        <Sort
-            :sortList="sortList"
-            :sortByUpDown="sortByUpDown"
-            @changeSort="(n) => changeSortKey(n)"
-            @changeByUpDown="() => changeSortUpDown()"
-        />
-    </div>
-    <Table :columnsRus="columnsRus" :data="data" />
-    <div class="text-3xl text-center">
-        <RouterView />
-    </div>
+  <Header />
+  <Loader :loading="loading" />
+  <div>
+    <Sort
+      :sortList="sortList"
+      :sortByUpDown="sortByUpDown"
+      @changeSort="(n) => changeSortKey(n)"
+      @changeByUpDown="() => changeSortUpDown()"
+    />
+    <Search />
+  </div>
+  <Table :columnsRus="columnsRus" :data="data" />
+  <div class="text-3xl text-center">
+    <RouterView />
+  </div>
 </template>
